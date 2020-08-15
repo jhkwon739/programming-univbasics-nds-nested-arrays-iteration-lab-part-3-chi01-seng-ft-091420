3d5616ead35b1757d269a6e702be5e89a25d3269
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-15 03:34:00 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,13 +7,11 @@ def join_nested_strings(src)
     element_index = 0 
     while element_index < src[row_index].count do
       if src[row_index][element_index].is_a?(String)
-        p src[row_index][element_index]
         joined_string_array.push(src[row_index][element_index])
       end
       element_index += 1
     end
     row_index += 1
   end
-  p joined_string_array
   p joined_string_array.join(" ")
 end
