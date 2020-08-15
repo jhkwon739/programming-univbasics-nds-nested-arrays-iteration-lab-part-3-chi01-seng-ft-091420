11c39ca6b8123e5b43b207d639896d19d7857e9b
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-15 03:26:39 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,4 +3,10 @@ def join_nested_strings(src)
   # Combine all Strings present in the AoA into a single value and return it
   joined_string_array = []
   row_index = 0
+  while row_index < src.count do
+    element_index = 0 
+    while element_index < src[row_index].count do
+      if src[row_index][element_index].class == STRING 
+    end
+  end
 end
